551321148ec4b4f5bb4cdaccd88fa4e03ff85df7
kevin <kevin.grundhall01@gmail.com>
2026-09-06 18:02:52 +0200

    AST.cpp, print_ast()

---
+++ include/AST.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "Parser.h"
+
+void print_ast(const Expr* expr);
---
+++ src/parser/AST.cpp
@@ -0,0 +1,48 @@
+#include "AST.h"
+#include <variant>
+#include <memory>
+#include <iostream>
+#include <string>
+
+void print_ast(const Expr* expr)
+{
+    if (!expr) return;
+
+    // Literal
+    if (auto l = dynamic_cast<const Literal*>(expr))
+    {
+        if (std::holds_alternative<double>(l->value))
+            std::cout << std::get<double>(l->value);
+
+        else if (std::holds_alternative<std::string>(l->value))
+            std::cout << std::get<std::string>(l->value);
+
+        else if (std::holds_alternative<std::monostate>(l->value))
+            std::cout << "null";
+    }
+    // Unary ( -x )
+    else if (auto u = dynamic_cast<const Unary*>(expr))
+    {
+        std::cout << "(" << u->op.lexeme << " ";
+        print_ast(u->right.get());
+        std::cout << ")";
+    }
+
+    // Binary ( x + y )
+    else if (auto b = dynamic_cast<const Binary*>(expr))
+    {
+        std::cout << "(" << b->op.lexeme << " ";
+        print_ast(b->left.get());
+        std::cout << " ";
+        print_ast(b->right.get());
+        std::cout << ")";
+    }
+
+    // Grouping ( (x) )
+    else if (auto g = dynamic_cast<const Grouping*>(expr))
+    {
+        std::cout << "(group ";
+        print_ast(g->expression.get());
+        std::cout << ")";
+    }
+}
